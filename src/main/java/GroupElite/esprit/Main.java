74721791135ff4a7d312fa890d23d183c6353876
faroukazizi1 <azizi.farouk@esprit.tn>
2025-02-08 15:11:16 +0100

    First Commit

---
+++ src/main/java/GroupElite/esprit/Main.java
@@ -0,0 +1,13 @@
+package GroupElite.esprit;
+
+import java.sql.Connection;
+
+import Util.DBConnection;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Util/DBConnection.java
@@ -0,0 +1,39 @@
+package Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private String url = "jdbc:mysql://localhost:3306/management_user";
+    private String user = "root";
+    private String password = "";
+    private Connection conn;
+    private static DBConnection instance;
+
+    public static DBConnection getInstance() {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    private DBConnection() {
+        try {
+            this.conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection established");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+
+}
