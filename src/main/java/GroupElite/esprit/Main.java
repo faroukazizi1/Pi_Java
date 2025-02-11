88339f92d916b0ec8ea145c437a5f01b2ae4dbc0
faroukazizi1 <azizi.farouk@esprit.tn>
2025-02-11 14:17:28 +0100

    Modifier fichier Main.java pour pouvoir merge main avec Gestion_user

--- src/main/java/GroupElite/esprit/Main.java
+++ src/main/java/GroupElite/esprit/Main.java
@@ -1,30 +1,12 @@
 package GroupElite.esprit;
-import Model.Formation;
-import Service.FormationService;
-import Model.Formateur;
-import Service.FormateurService;
 
 import java.sql.Connection;
-import java.sql.Date;
 
 import Util.DBConnection;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        FormationService fo=new FormationService();
-        Date Date_D = Date.valueOf("2025-04-01");  // Format yyyy-mm-dd
-        Date Date_F = Date.valueOf("2025-04-10");
-        fo.add(new Formation("mm","kedheugyujf",Date_F,Date_D,20,"havxjhvaxkabkxv", 1));
-        fo.delete(new Formation(42,"kedhe","kedheugyujf",Date_F,Date_D,20,"havxjhvaxkabkxv", 1));
-        fo.update(new Formation(1,"teambulding","kedheugyujf",Date_F,Date_D,20,"havxjhvaxkabkxv", 1));
-        System.out.println(fo.getAll());
-        FormateurService formateurS = new FormateurService();
-        formateurS.add(new Formateur(24,"mohamed","amine ","sai.lfft@esprit.com","math"));
-        formateurS.delete(new Formateur(27,24,"menyar","ben ghorbel","said.laffet@esprit.com","math"));
-        formateurS.update(new Formateur(28,24,"manou","ben ghorbel","said.laffet@esprit.com","math"));
-        System.out.println(formateurS.getAll());
 
     }
 }
